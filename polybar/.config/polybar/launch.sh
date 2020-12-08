4b9bb751c68707e6987a2d8b4a6541984a6c7bf9
Namit Nathwani <namsnath@gmail.com>
2020-12-08 21:53:32 +0530

    update: polybar theming - overhaul
    - Renames config to config.ini
    - Moves colors to colors.ini
    - Moves modules to modules.ini
    - Defines shared bar properties
    - Cleans up main config
    - Updates launch with path to config file

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,13 +6,15 @@ killall -q polybar
 # Wait until processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
+CONF_PATH="~/.config/polybar/config.ini"
+
 # Launch polybar using config
-polybar top_laptop &
+polybar --config=$CONF_PATH top_laptop &
 
 EXTERNAL_CONNECTED=$(xrandr | grep -o "HDMI-1 connected")
 echo "$EXTERNAL_CONNECTED"
 
 if [[ $EXTERNAL_CONNECTED ]]; then
 	echo "External display connected"
-	polybar top_external &
+	polybar --config=$CONF_PATH top_external &
 fi
